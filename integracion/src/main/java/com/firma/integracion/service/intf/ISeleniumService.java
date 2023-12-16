4ece43279fcdecc0dbb4a6e269b3ee5c30e880e9
Juan.Paez <jdpluc302@gmail.com>
2023-12-15 19:10:12 -0500

    move folders out of the project

--- integracion/src/main/java/com/firma/integracion/service/intf/ISeleniumService.java
+++
@@ -1,8 +0,0 @@
-package com.firma.integracion.service.intf;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ISeleniumService {
-    public String getUrlJuzgado(String nameDespacho, String urlJuzgado) throws InterruptedException;
-}
