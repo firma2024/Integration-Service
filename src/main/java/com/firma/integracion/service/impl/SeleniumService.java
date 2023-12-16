38bc95194c07ee01863f8ee00f7c2fd45fe35583
Juan.Paez <jdpluc302@gmail.com>
2023-12-15 20:49:11 -0500

    update docker

--- src/main/java/com/firma/integracion/service/impl/SeleniumService.java
+++ src/main/java/com/firma/integracion/service/impl/SeleniumService.java
@@ -23,9 +23,9 @@ public class SeleniumService implements ISeleniumService {
         List<String> wordsList = new ArrayList<>(Arrays.asList(nameDespacho.split("\\s+")));
         String city = wordsList.get(wordsList.size() - 1);
 
-        ChromeOptions options = new ChromeOptions();
+        FirefoxOptions options = new FirefoxOptions();
         options.addArguments("--headless"); // Ejecutar en modo headless
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new FirefoxDriver(options);
 
         driver.get(urlJuzgado);
 
