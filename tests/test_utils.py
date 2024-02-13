a6cb85efd01f6bdfe3b390d30e441d14c90782e8
Juan Páez <jdpluc302@gmail.com>
2024-02-12 19:06:55 -0500

    unit test format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,21 +2,23 @@ import unittest
 from parameterized import parameterized
 from utils.utils import *
 
+
 class TestUtils(unittest.TestCase):
     @parameterized.expand(
         [
-            [   "no_modifications",
+            [
+                "no_modifications",
                 "JUZGADO 001 CIVIL MUNICIPAL DE BELLO",
                 "JUZGADO 001 CIVIL MUNICIPAL DE BELLO",
             ],
-            [   
+            [
                 "with_modifications",
                 "JUZGADO 030 ADMINISTRATIVO  DE LA SECCION SEGUNDA  DE BOGOTÁ ",
                 "JUZGADO 030 ADMINISTRATIVO DE LA SECCION SEGUNDA DE BOGOTÁ",
             ],
         ],
     )
-    def test_clean_string(self,name, office_name, expected):
+    def test_clean_string(self, name, office_name, expected):
         self.assertEqual(clean_string(office_name), expected)
 
     def test_get_defendant_and_plaintiff(self):
@@ -27,17 +29,32 @@ class TestUtils(unittest.TestCase):
 
     @parameterized.expand(
         [
-            [   "no_split",
-                [1,2,3,4],
-                1,
-                [[1,2,3,4]]
-            ],
-            [   "split",
-                [1,2,3,4],
-                2,
-                [[1,2],[3,4]]
-            ]
+            ["no_split", [1, 2, 3, 4], 1, [[1, 2, 3, 4]]],
+            ["split", [1, 2, 3, 4], 2, [[1, 2], [3, 4]]],
         ],
     )
-    def test_split_list(self,name,lst,n,expected):
+    def test_split_list(self, name, lst, n, expected):
         assert list(split_list(lst, n)) == expected
+
+    def test_replace_placeholders_email(self):
+        html = """{{ id }}, {{ demandante }}, 
+                  {{ demandado }}, {{ actuacion }}, 
+                  {{ radicado }}, {{ anotacion }}, 
+                  {{ fechaActuacion }}, {{ emailAbogado }}, 
+                  {{ nameAbogado }}, {{ link }}"""
+        
+        actuacion_email = ActuacionEmail(
+            id=1,
+            demandante="Demandante Ejemplo",
+            demandado="Demandado Ejemplo",
+            actuacion="Actuacion Ejemplo",
+            radicado="Radicado Ejemplo",
+            anotacion="Anotacion Ejemplo",
+            fechaActuacion="Fecha Ejemplo",
+            emailAbogado="Email Ejemplo",
+            nameAbogado="Nombre Abogado Ejemplo",
+            link="Link Ejemplo"
+        )
+        res = replace_placeholders_email(
+            html,actuacion_email
+        )
