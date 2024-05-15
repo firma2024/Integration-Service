9832d9e5813315d8cbb57a5c34412c9992d338ef
paez49 <jdpluc302@gmail.com>
2024-05-14 22:31:59 -0500

    update urls

--- constants/constants.py
+++ constants/constants.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-URL_CPNU = "https://consultaprocesos.portalhistorico.ramajudicial.gov.co:448/api/v2/Procesos/Consulta/NumeroRadicacion?numero="
-URL_CPNU_SINGLE = "https://consultaprocesos.portalhistorico.ramajudicial.gov.co:448/api/v2/Proceso/"
-URL_CPNU_ACTUACIONES = "https://consultaprocesos.portalhistorico.ramajudicial.gov.co:448/api/v2/Proceso/Actuaciones/"
-URL_RAMA_JUDICIAL="https://portalhistorico.ramajudicial.gov.co/portal/inicio"
-URL_RAMA_JUDICIAL_INICIO="https://portalhistorico.ramajudicial.gov.co"
-=======
 URL_CPNU = "https://consultaprocesos.ramajudicial.gov.co:448/api/v2/Procesos/Consulta/NumeroRadicacion?numero="
 URL_CPNU_SINGLE = "https://consultaprocesos.ramajudicial.gov.co:448/api/v2/Proceso/"
 URL_CPNU_ACTUACIONES = "https://consultaprocesos.ramajudicial.gov.co:448/api/v2/Proceso/Actuaciones/"
 URL_RAMA_JUDICIAL="https://portalhistorico.ramajudicial.gov.co/portal/inicio"
 URL_RAMA_JUDICIAL_INICIO="https://portalhistorico.ramajudicial.gov.co"
->>>>>>> fc93f0a4b5259eadc48bf376f64bc50aa4b1e9d5
